7d10670cb82c9f3177db2d4e8f99adb199bc5ec3
Jose VIllaro <jvillaro@gmail.com>
2019-12-19 11:23:48 -0300

    iOS setup

---
+++ Template.iOS/Services/PlatformService.cs
@@ -0,0 +1,15 @@
+﻿#region --- using ---
+
+using Template.Core.Interfaces;
+
+#endregion
+
+namespace Template.iOS.Services
+{
+    /// <summary>
+    /// iOS implementation of the Platform Service
+    /// </summary>
+    public class PlatformService : IPlatformService
+    {
+    }
+}
---
+++ Template.iOS/Setup.cs
@@ -0,0 +1,41 @@
+﻿#region --- usings ---
+
+using MvvmCross.Forms.Platforms.Ios.Core;
+using MvvmCross.Logging;
+using Template.Core.Interfaces;
+using Template.iOS.Services;
+using MvvmCross;
+using MvvmCross.Base;
+using MvvmCross.Plugin.Json;
+
+#endregion
+
+namespace Template.iOS
+{
+    public class Setup : MvxFormsIosSetup<Core.App, Core.UI.App>
+    {
+        /// <summary>
+        /// Sets the log provider
+        /// </summary>
+        /// <returns></returns>
+        public override MvxLogProviderType GetDefaultLogProviderType()
+            => MvxLogProviderType.Console;
+
+
+        //protected override IMvxLogProvider CreateLogProvider()
+        //{
+        //    return new MvxLogProvider();
+        //}
+
+
+        /// <summary>
+        /// Initializes the platform services
+        /// </summary>
+        protected override void InitializeFirstChance()
+        {            
+            Mvx.IoCProvider.RegisterSingleton<IMvxJsonConverter>(new MvxJsonConverter());
+            Mvx.IoCProvider.RegisterSingleton<IPlatformService>(new PlatformService());
+            base.InitializeFirstChance();
+        }
+    }
+}
